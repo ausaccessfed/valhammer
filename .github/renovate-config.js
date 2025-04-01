e80c199540602d7a73b62239dec27da22af7a2ad
aaf-terraform <118229371+aaf-terraform@users.noreply.github.com>
2025-04-01 13:29:12 +0800

    Updating distribution files (#48)

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,5 +1,29 @@
 module.exports = {
     "extends": [
         "github>ausaccessfed/workflows//.github/renovate-config.json"
-    ]
+    ],
+    "username": "aaf-terraform",
+    "gitAuthor": "aaf-terraform <fishwhack9000+terraform@gmail.com>",
+    "onboarding": false,
+    "requireConfig": "optional",
+    "automerge": false,
+    "ignoreTests": false,
+    "platform": "github",
+    "forkProcessing": "disabled",
+    "labels": [
+        "dependencies"
+    ],
+    "lockFileMaintenance": {
+        "enabled": false
+    },
+    "ignorePaths": [
+        "**/node_modules/**",
+        "**/bower_components/**",
+        "**/vendor/**",
+        "**/examples/**",
+        "**/__tests__/**",
+        "**/tests/**",
+        "**/__fixtures__/**",
+        "**/.terraform/**"
+    ],
 }
