6dcfd49380255a5190397f0e67e11cd9ad65f71d
aaf-terraform <118229371+aaf-terraform@users.noreply.github.com>
2025-04-17 08:44:00 +0800

    Updating distribution files (#49)

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const common = {
     "extends": [
         "github>ausaccessfed/workflows//.github/renovate-config.json"
     ],
@@ -27,3 +27,5 @@ module.exports = {
         "**/.terraform/**"
     ],
 }
+//EOF_DISTRIBUTION
+module.exports = {
